8a48651ebf2260b63b6ef34bafe7eb5a4cfb581e
Martin Albrecht <martinralbrecht@googlemail.com>
2013-10-10 23:12:38 +0200

    working PGP/MIME implementation

--- pgpmime/pgpmime.py
+++ pgpmime/pgpmime.py
@@ -1,108 +1,77 @@
+"""
+Based on https://pypi.python.org/pypi/pgp-mime/
+"""
 from email import Message
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.encoders import encode_7or8bit
-
 from email import encoders
-from email.message import Message
-from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
-from email.mime.image import MIMEImage
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 import os
 import mimetypes
 
-class PGPMimeMessage(object):
-    def __init__(self, to, cc=None, body=None, attachments=None):
-        self.to = tuple(to)
-        self.cc = tuple(cc) if cc else tuple()
-
-        body = MIMEText(body.encode('utf-8'), 'plain', 'utf-8')
-        body.add_header('Content-Disposition', 'inline')
-
-        if attachments is None:
-            self.msg = body
-        else:
-            self.msg = MIMEMultipart()
-            self.msg.preamble = 'This is a multi-part message in MIME format.'
-
-            self.msg.attach(body)
-
-            for filename in attachments:
-                path = os.path.abspath(filename)
-                if not os.path.isfile(path):
-                    continue
-                # Guess the content type based on the file's extension.  Encoding
-                # will be ignored, although we should check for simple things like
-                # gzip'd or compressed files.
-                ctype, encoding = mimetypes.guess_type(path)
-                if ctype is None or encoding is not None:
-                    # No guess could be made, or the file is encoded (compressed), so
-                    # use a generic bag-of-bits type.
-                    ctype = 'application/octet-stream'
-                maintype, subtype = ctype.split('/', 1)
-
-                if maintype == 'text':
-                    fp = open(path)
-                    # Note: we should handle calculating the charset
-                    msg = MIMEText(fp.read(), _subtype=subtype)
-                    fp.close()
-
-                elif maintype == 'image':
-                    fp = open(path, 'rb')
-                    msg = MIMEImage(fp.read(), _subtype=subtype)
-                    fp.close()
-
-                elif maintype == 'audio':
-                    fp = open(path, 'rb')
-                    msg = MIMEAudio(fp.read(), _subtype=subtype)
-                    fp.close()
-
-                elif maintype == 'application':
-                    fp = open(path, 'rb')
-                    msg = MIMEApplication(fp.read(),_subtype=subtype, _encoder=encoders.encode_base64)
-                    fp.close()
-
-                else:
-                    fp = open(path, 'rb')
-                    msg = MIMEBase(maintype, subtype)
-                    msg.set_payload(fp.read())
-                    fp.close()
-                    # Encode the payload using Base64
-                    encoders.encode_base64(msg)
-
-                # Set the filename parameter
-                msg.add_header('Content-Disposition', 'attachment', filename=os.path.basename(path))
-                self.msg.attach(msg)
-        self.msg.epilogue = ''
-
-    def __call__(self, signer):
-        sig = self.sign(signer)
-
-        msg = MIMEMultipart('signed', micalg='pgp-sha1',
-                            protocol='application/pgp-signature')
-        msg.attach(self.msg)
-        msg.attach(sig)
-        return msg
-
-    def sign(self, signer):
-        from gnupg import gpgobj
-
-        body = flatten(self.msg, fix_cr=True)
-        signature = gpgobj.msg_sign(body, signer)
-        sig = MIMEApplication(_data=signature,
-                              _subtype='pgp-signature; name="signature.asc"',
-                              _encoder=encode_7or8bit)
-        sig['Content-Description'] = 'This is a digital signature.'
-        sig.set_charset('us-ascii')
-        return sig
+from paleca.gnupg import gpgobj as gnupg
 
 
-def flatten(msg, fix_cr=False):
+
+class PGPMIMEsigned(MIMEMultipart):
+    def __init__(self, msg, signer=None):
+        if msg.is_multipart():
+            # we need these to get our message correctly parsed by KMail and Thunderbird
+            msg.preamble = 'This is a multi-part message in MIME format.'
+            msg.epilogue = '' 
+
+        if signer is not None:
+            msg_str = flatten(msg)
+            sig     = gnupg.msg_sign(msg_str, signer)
+            sig = MIMEApplication(_data=sig,
+                                  _subtype='pgp-signature; name="signature.asc"',
+                                  _encoder=encode_7or8bit)
+            sig['Content-Description'] = 'This is a digital signature.'
+            sig.set_charset('us-ascii')
+
+            #msg_ = MIMEMultipart('signed', micalg='pgp-sha1', protocol='application/pgp-signature')
+            MIMEMultipart.__init__(self, 'signed', micalg='pgp-sha1', protocol='application/pgp-signature')
+            self.attach(msg)
+            self.attach(sig)
+
+    def verify(self):
+        subparts = self.get_payload()
+        assert(len(subparts) == 2)
+        msg, sig = subparts
+        msg_str = flatten(msg)
+        res = gnupg.verify_signature(msg_str, sig.get_payload())
+        return res
+
+class PGPMIMEencrypted(MIMEMultipart):
+    def __init__(self, msg, recipients):
+
+        MIMEMultipart.__init__(self, 'encrypted', micalg='pgp-sha1', protocol='application/pgp-encrypted')
+
+        body = flatten(msg)
+        encrypted = gnupg.msg_encrypt(body, recipients)
+
+        payload = MIMEApplication(_data=encrypted,
+                                  _subtype='octet-stream; name="encrypted.asc"',
+                                  _encoder=encode_7or8bit)
+        payload['Content-Description'] = 'OpenPGP encrypted message'
+        payload.set_charset('us-ascii')
+
+        control = MIMEApplication(_data='Version: 1\n',
+                                  _subtype='pgp-encrypted',
+                                  _encoder=encode_7or8bit)
+        control.set_charset('us-ascii')
+        
+        self.attach(control)
+        self.attach(payload)
+        self['Content-Disposition'] = 'inline'
+
+def PGPMIME(msg, recipients, signer):
+    return PGPMIMEencrypted( PGPMIMEsigned(msg, signer), recipients)
+
+def flatten(msg):
     from cStringIO import StringIO
     from email.generator import Generator
     fp = StringIO()
@@ -110,7 +79,4 @@ def flatten(msg, fix_cr=False):
     g.flatten(msg)
     text = fp.getvalue()
 
-    if fix_cr:
-        return '\r\n'.join(text.splitlines()) + '\r\n'
-    else:
-        return text
+    return '\r\n'.join(text.splitlines()) + '\r\n'
