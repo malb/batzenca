3a034db59d67fd5a00676cea9c87eba4b284e0e4
Martin Albrecht <martinralbrecht@googlemail.com>
2013-12-19 18:43:07 +0000

    Peer in MailingList

--- batzenca/database/mailinglists.py
+++ batzenca/database/mailinglists.py
@@ -2,7 +2,8 @@ from sqlalchemy import Column, Integer, String, Date, ForeignKey, UnicodeText
 from sqlalchemy.orm import relationship
 
 from base import Base, EntryNotFound
-from releases import Release
+from releases import Release, ReleaseKeyAssociation
+from keys import Key
 
 import warnings
 
@@ -61,3 +62,33 @@ class MailingList(Base):
             return inherit.inherit(date=date, deactivate_invalid=deactivate_invalid, delete_old_inactive_keys=delete_old_inactive_keys)
         else:
             return Release(mailinglist=self, date=date, active_keys=[], inactive_keys=[], policy=self.policy)
+
+    def __contains__(self, obj):
+        """Return ``True`` if ``obj`` was ever in any release for this mailing list.
+
+        INPUT:
+
+        - ``obj`` - either an object of type :class:`batzenca.database.peers.Peer` or
+          :class:`batzenca.database.keys.Key`
+
+        """
+        from batzenca.session import session
+        from sqlalchemy.sql import or_
+        
+        query = session.db_session.query(Release).filter(Release.mailinglist == self)
+        query = query.join(ReleaseKeyAssociation).filter(ReleaseKeyAssociation.right_id == Release.id)
+        query = query.join(Key)
+
+        if isinstance(obj, Peer):
+            q = [ReleaseKeyAssociation.left_id == key.id for key in obj.keys]
+            query = query.filter(or_(*q))
+        elif isinstance(obj, Key):
+            query = query.filter(ReleaseKeyAssociation.left_id == obj.id)
+        else:
+            raise TypeError("Object of type '%s' passed, but only types `Peer` and `Key` are supported."%(type(obj)))
+            
+        if query.first() is not None:
+            return True
+        else:
+            return False
+            
