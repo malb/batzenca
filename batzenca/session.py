ff241c206ce7fed33bbcb14ec5cf663fa0a5dcc8
Martin Albrecht <martinralbrecht@googlemail.com>
2014-05-03 18:24:18 +0100

    adding git snapshots of BATZENCADIR

--- batzenca/session.py
+++ batzenca/session.py
@@ -3,9 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 import os
+import git
 
 
 class Session:
+
+    _gitignore = """.gitignore
+gnupg/pubring.gpg~
+gnupg/random_seed
+releases
+"""
+
     def __init__(self, path):
         self.path = path
         if not os.path.exists(path):
@@ -15,6 +23,13 @@ class Session:
         if not os.path.isdir(path):
             raise IOError("Cannot create configuration directory '%' because a file with the same name exists already."%path)
 
+        if not os.path.exists(self.release_dump_path):
+            os.mkdir(self.release_dump_path)
+            os.chmod(self.release_dump_path, 0700)
+
+        if not os.path.isdir(self.release_dump_path):
+            raise IOError("Cannot create directory '%' because a file with the same name exists already."%self.release_dump_path)
+            
         self.db_engine = create_engine('sqlite:///%s/batzenca.db'%path, echo=False)
 
         Base.metadata.create_all(self.db_engine)
@@ -25,8 +40,23 @@ class Session:
         from gnupg import GnuPG
         self.gnupg = GnuPG(path + os.path.sep + "gnupg")
 
-    def commit(self):
+        try:
+            git.Repo(self.path)
+        except git.InvalidGitRepositoryError:
+            repo = git.Repo.init(self.path)
+            # git ignore
+            open(os.path.join(repo.working_dir, ".gitignore"), "w").write(Session._gitignore)
+            # add useful files
+            repo.git.add("batzenca.db")
+            repo.git.add("gnupg/pubring.gpg")
+            repo.git.add("gnupg/secring.gpg")
+            repo.git.add("gnupg/trustdb.gpg")
+            repo.git.commit(m="initial commit")
+            
+    def commit(self, snapshot=False, *args, **kwds):
         self.db_session.commit()
+        if snapshot:
+            self.snapshot(*args, **kwds)
 
     @property
     def query(self):
@@ -44,7 +74,18 @@ class Session:
     def release_dump_path(self):
         """Path for storing releases in .asc and .yaml format."""
         return os.path.join(self.path, "releases")
-        
+
+    def snapshot(self, verbose=False, msg="snapshot"):
+        repo = git.Repo(self.path)
+        repo.git.add("batzenca.db")
+        repo.git.add("gnupg")
+        if verbose:
+            print repo.git.status()
+        if repo.is_dirty():
+            out = repo.git.commit(m=msg)
+            if verbose:
+                print out
         
 BATZENCADIR  = os.environ.get("BATZENCADIR", os.path.expanduser("~") + os.path.sep + ".batzenca")
 session = Session(BATZENCADIR)
+
