6420512374fad9304bb7c3156bcaa113ba2ed75f
Martin R. Albrecht <martinralbrecht@googlemail.com>
2019-01-20 12:07:48 +0000

    there is no unicode()

--- batzenca/database/policies.py
+++ batzenca/database/policies.py
@@ -126,7 +126,7 @@ class Policy(Base):
             ``key``'s length is too short.
         """
         if len(key) < self.key_len:
-            msg = u"Key '%s' has key length %d but at least %d is required by '%s'."%(unicode(key), len(key), self.key_len, unicode(self))
+            msg = u"Key '%s' has key length %d but at least %d is required by '%s'."%(key, len(key), self.key_len, self)
             warnings.warn(msg, PolicyViolation)
             return False
         return True
@@ -152,7 +152,7 @@ class Policy(Base):
         if not key_algorithms.issubset(algorithms):
             diff = key_algorithms.difference(algorithms)
             diff_str = ",".join(session.gnupg.alg_to_str[e] for e in diff)
-            msg = u"Key '%s' uses algorithm(s) '%s' which is/are not in '%s' as mandated by '%s'."%(unicode(key), diff_str, self.algorithms_str, unicode(self))
+            msg = u"Key '%s' uses algorithm(s) '%s' which is/are not in '%s' as mandated by '%s'."%(key, diff_str, self.algorithms_str, self)
             warnings.warn(msg, PolicyViolation)
             return False
         return True
@@ -168,13 +168,13 @@ class Policy(Base):
             ``key``'s length is too short.
         """
         if not key.expires and self.key_lifespan > 0:
-            msg = u"Key '%s'does not expire but expiry of %d days is mandated by '%s'."%(unicode(key), self.key_lifespan, unicode(self))
+            msg = u"Key '%s'does not expire but expiry of %d days is mandated by '%s'."%(key, self.key_lifespan, self)
             warnings.warn(msg, PolicyViolation)
             return False
         else:
             max_expiry = datetime.date.today() + datetime.timedelta(days=self.key_lifespan)
             if max_expiry < key.expires:
-                msg = u"Key '%s' expires on %s but max allowed expiration date is %s."%(unicode(key), key.expires, max_expiry)
+                msg = u"Key '%s' expires on %s but max allowed expiration date is %s."%(key, key.expires, max_expiry)
                 warnings.warn(msg, PolicyViolation)
                 return False
             return True
@@ -190,7 +190,7 @@ class Policy(Base):
             ``key``'s length is too short.
         """
         if not key.is_signed_by(self.ca):
-            msg = u"No UID of Key '%s' has a valid signature of the CA key '%s'"%(unicode(key), unicode(self.ca))
+            msg = u"No UID of Key '%s' has a valid signature of the CA key '%s'"%(key, self.ca)
             warnings.warn(msg, PolicyViolation)
             return False
         return True
